6e6715baa41f5f56042a08f9889370ec5fd1c088
nogluzia09 <127217294+nogluzia09@users.noreply.github.com>
2024-09-25 16:41:33 -0300

    m

---
+++ graficos/common.js
@@ -0,0 +1,14 @@
+const getCSS=(variavel)=> {
+   const bodyStyles.getpropertyValue(variavel)
+   return bodyStyles.getpropertyValue(variavel)
+
+}
+
+const tickConfig={
+    family: getCSS('--font'),
+    size:16,
+    color: getCSS('--primary-color')
+}
+
+export {grtCSS, tickConfig}
+
