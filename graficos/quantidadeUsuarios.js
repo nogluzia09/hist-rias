f574d9396f7481a1837385ea28f829287a114028
nogluzia09 <127217294+nogluzia09@users.noreply.github.com>
2024-10-09 14:50:08 -0300

    n

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,8 +1,20 @@
 import{ getCSS,tickConfig } from "./common.js"
+
 async function quantidadeUsuariosPorRede() {
     const url ='https://.githubusercontent.com/guilhermeonrails/api/main/'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = await res.json()
-    const
+    const quantidadeDeUsuarios = Object.values(dados)
+
+    const data =[
+      {
+        x: nomeDasRedes,
+        y: quantidadeDeUsuarios,
+        
+
+      }  
+     ]
+
+
 }
